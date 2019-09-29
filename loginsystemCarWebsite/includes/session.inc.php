bcc8f9af45915f8d138a5db06ff24695393ebf9a
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2019-09-29 11:43:12 +0100

    updated session php

--- loginsystemCarWebsite/includes/session.inc.php
+++ loginsystemCarWebsite/includes/session.inc.php
@@ -1,5 +1,5 @@
 
-<!-- Check this page --> 
+<!-- Check this page -->
 
 <?php
 include('dbh.inc.php');
@@ -7,7 +7,7 @@ session_start();
 //checking whether user is logged in
 $user_check = $_SESSION["login_user"];
 //selecting user from user table in database
-$ses_sql = mysqli_query($conn,"SELECT userID FROM user WHERE userID = '$user_check' ");
+$ses_sql = mysqli_query($conn,"SELECT idUsers FROM users WHERE idUsers = '$user_check' ");
 $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 //if a user isn't logged it, it will redirect them to the sign in page
 if(!isset($_SESSION["login_user"])){
